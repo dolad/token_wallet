604df1a1adaed99b212703c6f92a7a8b8880e8ba
dolad <adavidoladele@gmail.com>
2021-04-06 12:58:16 +0100

    adding mint coin

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,7 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     uint8 private _decimal = 5;
+    uint256 private allocate_token = 1000;
+    
     constructor() ERC20 ("DavidToken", "DTN") {
+        _mint(msg.sender, allocate_token);
     }   
     function decimals() override public view returns (uint8) {
         return _decimal;
