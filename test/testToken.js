31c672bcc5e2fa737be2479746ce63047efe5574
dolad <adavidoladele@gmail.com>
2021-04-18 23:02:21 +0100

    mt test failing

--- test/testToken.js
+++ test/testToken.js
@@ -4,8 +4,16 @@ contract("myToken", async account => {
     beforeEach(async () => {
        contractInstance = await myToken.deployed()
      })
+     it("should return the name", async () => {
+        const returnName = await contractInstance("David");
+         assert.equal(returnName, "David")
+     } );
+     it("should return the tokenName", async () => {
+        const returnName = await contractInstance("TKN");
+         assert.equal(returnName, "TKN")
+     } );
     it("should return decimal", async () => {
        const returnDecimal = await contractInstance.decimals();
         assert.equal(returnDecimal,10000)
-    } )
+    } );
 })
